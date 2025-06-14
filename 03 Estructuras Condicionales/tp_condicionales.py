c2bc44288ea2c4241677838863b25d051a266347
topias1903 <tobias_alamedas@hotmail.com>
2025-06-13 23:49:40 -0300

    Se agrega archivo .py con las resoluciones del TP

---
+++ 03 Estructuras Condicionales/tp_condicionales.py
@@ -0,0 +1,143 @@
+#1) Escribir un programa que solicite la edad del usuario. Si el usuario es mayor de 18 años,
+#deberá mostrar un mensaje en pantalla que diga “Es mayor de edad”.
+
+edad = int(input("Ingrese su edad: "))
+
+if edad > 18:
+    print("Es mayor de edad")
+else:
+    print("Es menor de edad")
+
+# 2) Escribir un programa que solicite su nota al usuario. Si la nota es mayor o igual a 6, deberá
+# mostrar por pantalla un mensaje que diga “Aprobado”; en caso contrario deberá mostrar el
+# mensaje “Desaprobado”.
+
+nota = float(input("Ingrese su nota: "))
+
+if nota >= 6:
+    print("Aprobado")
+else:
+    print("Desaprobado")
+
+# 3) Escribir un programa que permita ingresar solo números pares. Si el usuario ingresa un
+# número par, imprimir por en pantalla el mensaje "Ha ingresado un número par"; en caso
+# contrario, imprimir por pantalla "Por favor, ingrese un número par". Nota: investigar el uso del
+# operador de módulo (%) en Python para evaluar si un número es par o impar.
+
+numero = int(input("Ingrese un numero: "))
+
+if numero % 2 == 0:
+    print("Ha ingresado un numero par!")
+else:
+    print("Por favor, ingrese un numero par")
+
+# 4) Escribir un programa que solicite al usuario su edad e imprima por pantalla a cuál de las
+# siguientes categorías pertenece:
+# ● Niño/a: menor de 12 años.
+# ● Adolescente: mayor o igual que 12 años y menor que 18 años.
+# ● Adulto/a joven: mayor o igual que 18 años y menor que 30 años.
+# ● Adulto/a: mayor o igual que 30 años.
+
+edad = int(input("Ingrese su edad: "))
+
+if edad >= 12 and edad < 18:
+    print("Usted es adolescente")
+elif edad >= 18 and edad < 30:
+    print("Usted es adulto joven")
+elif edad >= 30:
+    print("Usted es adulto")
+else:
+    print("Usted es niño")
+
+# 5) Escribir un programa que permita introducir contraseñas de entre 8 y 14 caracteres
+# (incluyendo 8 y 14). Si el usuario ingresa una contraseña de longitud adecuada, imprimir por en
+# pantalla el mensaje "Ha ingresado una contraseña correcta"; en caso contrario, imprimir por
+# pantalla "Por favor, ingrese una contraseña de entre 8 y 14 caracteres". Nota: investigue el uso
+# de la función len() en Python para evaluar la cantidad de elementos que tiene un iterable tal
+# como una lista o un string.
+
+contraseña = input("Ingrese una contraseña de entre 8 y 14 caracteres (incluyendo 8 y 14): ")
+
+if len(contraseña) >= 8 and len(contraseña) <= 14:
+    print("Ha ingresado una contraseña correcta")
+else:
+    print("Por favor, ingrese una contraseña de entre 8 y 14 caracteres")
+
+# 6) El paquete statistics de python contiene funciones que permiten tomar una lista de números
+# y calcular la moda, la mediana y la media de dichos números.
+# Teniendo en cuenta lo antes mencionado, escribir un programa que tome la lista
+# numeros_aleatorios, calcule su moda, su mediana y su media y las compare para determinar si
+# hay sesgo positivo, negativo o no hay sesgo. Imprimir el resultado por pantalla.
+# Definir la lista numeros_aleatorios de la siguiente forma:
+
+from statistics import mode, median, mean
+import random
+
+mi_lista = [random.randint(1, 100) for i in range(50)]
+
+moda = mode(mi_lista)
+mediana = median(mi_lista)
+media = mean(mi_lista)
+
+print(f"Lista: {mi_lista}")
+print(f"Moda: {moda}")
+print(f"Mediana: {mediana}")
+print(f"Media: {media}")
+
+if media > mediana and mediana > moda:
+    print("Sesgo positivo o a la derecha")
+elif media < mediana and mediana < moda:
+    print("Sesgo negativo o a la izquierda")
+else:
+    print("Sin sesgo")
+
+# 8) Escribir un programa que solicite al usuario que ingrese su nombre y el número 1, 2 o 3
+# dependiendo de la opción que desee:
+# 1. Si quiere su nombre en mayúsculas. Por ejemplo: PEDRO.
+# 2. Si quiere su nombre en minúsculas. Por ejemplo: pedro.
+# 3. Si quiere su nombre con la primera letra mayúscula. Por ejemplo: Pedro.
+# El programa debe transformar el nombre ingresado de acuerdo a la opción seleccionada por el
+# usuario e imprimir el resultado por pantalla. Nota: investigue uso de las funciones upper(),
+# lower() y title() de Python para convertir entre mayúsculas y minúsculas.
+
+nombre = input("Ingrese su nombre: ")
+opcion = input("Elegí una opción:\n1. Nombre en MAYÚSCULAS\n2. Nombre en minúsculas\n3. Nombre con la primera letra en mayúscula\nIngresá 1, 2 o 3: ")
+
+if opcion == "1":
+    print(nombre.upper())
+elif opcion == "2":
+    print(nombre.lower())
+elif opcion == "3":
+    print(nombre.title())
+else: 
+    print("Opcion invalida.")   
+
+# 9) Escribir un programa que pida al usuario la magnitud de un terremoto, clasifique la
+# magnitud en una de las siguientes categorías según la escala de Richter e imprima el resultado
+# por pantalla:
+# ● Menor que 3: "Muy leve" (imperceptible).
+# ● Mayor o igual que 3 y menor que 4: "Leve" (ligeramente perceptible).
+# ● Mayor o igual que 4 y menor que 5: "Moderado" (sentido por personas, pero
+# generalmente no causa daños).
+# ● Mayor o igual que 5 y menor que 6: "Fuerte" (puede causar daños en estructuras
+# débiles).
+# ● Mayor o igual que 6 y menor que 7: "Muy Fuerte" (puede causar daños significativos).
+# ● Mayor o igual que 7: "Extremo" (puede causar graves daños a gran escala).
+
+magnitud = int(input("Ingrese la magnitud del terremoto: "))
+
+if magnitud < 3:
+    print("Muy leve.")
+if magnitud >= 3 and magnitud < 4:
+    print("Leve.")
+if magnitud >= 4 and magnitud < 5:
+    print("Moderado.")
+if magnitud >= 5 and magnitud < 6:
+    print("Fuerte.")
+if magnitud >= 6 and magnitud < 7:
+    print("Muy fuerte.")
+if magnitud >= 7:
+    print("Extremo") 
+
+
+
